3a60242a88c17bc6f02f9fdec1f7685f7f475516
郭朝顺 <657257897@qq.com>
2022-12-03 17:28:59 +0800

    [comments]   补充Ping工具 [jira_id] none [version]

--- 封装原生网络,解析json空值/web调用/ViewController.m
+++ 封装原生网络,解析json空值/web调用/ViewController.m
@@ -10,9 +10,16 @@
 #import "NSNumber+EqualString.h"
 #import "Person.h"
 #import "NetWork.h"
-#import "PingHelper.h"
+#import "ULPingHelper.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include<netdb.h>
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -21,11 +28,88 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    PingHelper *pingHelper = [[PingHelper alloc] init];
+    ULPingHelper *pingHelper = [[ULPingHelper alloc] init];
     pingHelper.host = @"www.baidu.com";
-    [pingHelper pingWithBlock:^(BOOL isSuccess, NSTimeInterval latency) {
 
+    [NSTimer scheduledTimerWithTimeInterval:3 repeats:YES block:^(NSTimer * _Nonnull timer) {
+        NSLog(@"开始");
+        [pingHelper pingWithBlock:^(BOOL isSuccess, NSString * _Nonnull ipString, NSTimeInterval latency) {
+            NSLog(@"完成 %d %@  %d ms",isSuccess,ipString,(int)latency);
+        }];
     }];
+
+
+//    [NSTimer scheduledTimerWithTimeInterval:3 repeats:YES block:^(NSTimer * _Nonnull timer) {
+//        [self dnsFun1];
+//        [self dnsFun3];
+//    }];
+
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+
+
+}
+
+// https://blog.csdn.net/weixin_33785972/article/details/87996257
+- (void)dnsFun3 {
+    Boolean result,bResolved;
+    CFHostRef hostRef;
+    CFArrayRef addresses = NULL;
+    NSMutableArray * ipsArr = [[NSMutableArray alloc] init];
+
+    CFStringRef hostNameRef = CFStringCreateWithCString(kCFAllocatorDefault, "www.baidu.com", kCFStringEncodingASCII);
+
+    hostRef = CFHostCreateWithName(kCFAllocatorDefault, hostNameRef);
+    CFAbsoluteTime start = CFAbsoluteTimeGetCurrent();
+    result = CFHostStartInfoResolution(hostRef, kCFHostAddresses, NULL);
+    if (result == TRUE) {
+        addresses = CFHostGetAddressing(hostRef, &result);
+    }
+    bResolved = result == TRUE ? true : false;
+
+    if(bResolved)
+    {
+        struct sockaddr_in* remoteAddr;
+        for(int i = 0; i < CFArrayGetCount(addresses); i++)
+        {
+            CFDataRef saData = (CFDataRef)CFArrayGetValueAtIndex(addresses, i);
+            remoteAddr = (struct sockaddr_in*)CFDataGetBytePtr(saData);
+
+            if(remoteAddr != NULL)
+            {
+                //获取IP地址
+                char ip[16];
+                strcpy(ip, inet_ntoa(remoteAddr->sin_addr));
+                NSString * ipStr = [NSString stringWithCString:ip encoding:NSUTF8StringEncoding];
+                [ipsArr addObject:ipStr];
+            }
+        }
+    }
+    CFAbsoluteTime end = CFAbsoluteTimeGetCurrent();
+    NSLog(@"33333 === ip === %@ === time cost: %0.3fs", ipsArr,end - start);
+    CFRelease(hostNameRef);
+    CFRelease(hostRef);
+}
+
+- (void)dnsFun1 {
+    CFAbsoluteTime start = CFAbsoluteTimeGetCurrent();
+    char   *ptr, **pptr;
+    struct hostent *hptr;
+    char   str[32];
+    ptr = "www.baidu.com";
+    NSMutableArray * ips = [NSMutableArray array];
+    if((hptr = gethostbyname(ptr)) == NULL) {
+        return;
+    }
+    for(pptr=hptr->h_addr_list; *pptr!=NULL; pptr++) {
+        NSString * ipStr = [NSString stringWithCString:inet_ntop(hptr->h_addrtype, *pptr, str, sizeof(str)) encoding:NSUTF8StringEncoding];
+        [ips addObject:ipStr?:@""];
+    }
+
+    CFAbsoluteTime end = CFAbsoluteTimeGetCurrent();
+    NSLog(@"22222 === ip === %@ === time cost: %0.3fs", ips,end - start);
+
 }
 
 - (void)testMalloc {
@@ -43,8 +127,8 @@
     [self loadData:nil];
 
     NSDictionary * dic = @{
-                           @"person":[[Person alloc]init]
-                           };
+        @"person":[[Person alloc]init]
+    };
     NSLog(@"%@",dic);
 
     NSLog(@"--------------");
@@ -73,7 +157,7 @@
     network.urlString = @"http://qz.test.internet.zhiwangyilian.com/api/client-community/client/community/hot/hotList";
     network.parameters = @{@"pageIndex":@"0",
                            @"pageSize":@"20",
-                           };
+    };
     network.success = ^(NSDictionary * data) {
         
     };
